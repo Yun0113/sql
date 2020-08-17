f699ec42798a3a65c4253cae2e3f8ae8eb8c0172
Yun0113 <69342165+Yun0113@users.noreply.github.com>
2020-08-17 10:30:23 +0800

    Add files via upload

---
+++ DML.sql
@@ -0,0 +1,14 @@
+-- 插入记录 
+ insert into 表名 (属性名1,属性名2) values ('数值','数值')
+
+-- 修改记录  
+
+update 表名 set 属性名1=取值1,属性名2=取值2 where 筛选条件
+
+-- 删除记录  
+
+update 表名 set 属性名1=取值1,属性名2=取值2 where 筛选条件
+
+-- 查询记录  
+
+select  属性列表 from 表名 where 条件表达式  group by 属性名1[having 条件表达式] order by 属性名2 dese
